5d8f34de3e3a7f093fcdbaf504fc4d7e57597872
Sheikh <kenneth.sheikh@hotmail.com>
2025-02-13 11:22:42 +0100

    added obstacles

--- JS/script.js
+++ JS/script.js
@@ -1,59 +1,81 @@
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
-//Drawuing a box
-//         x   y   Width height
-
-/* console.log(Math.random() > 0.5); // log false and true 50% of the time
-  //Math.random gives a random number from 0 to 0.9999999 */
+const button = document.querySelector("button");
 
 const player = {
-  x: 0,
-  y: 0,
-  speed: 1,
+  x: 175,
+  y: 200,
+  height: 50,
+  width: 50,
+  speed: -6,
 };
 
-const button = document.querySelector("button");
+const obstacle = {
+  x: 500,
+  y: 200,
+  height: 200,
+  width: 50,
+};
+
 button.addEventListener("click", () => {
-  player.x += 10;
-  player.y += 10;
-  c.clearRect(0, 0, 400, 400);
-  c.fillRect(player.x, player.y, 50, 50);
+  player.speed = -6;
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  c.fillRect(player.x, player.y, player.width, player.height);
 });
 
 const left = document.querySelector("#left");
-const rigth = document.querySelector("#right");
+const right = document.querySelector("#right");
 
 left.addEventListener("click", () => {
   player.speed = -1;
 });
-rigth.addEventListener("click", () => {
+
+right.addEventListener("click", () => {
   player.speed = 1;
 });
 
+function checkBounds() {
+  if (player.y > 350 || player.y < 0) {
+    console.log("You died");
+    player.y = 200;
+    player.x = 175;
+    player.speed = -6;
+  }
+
+  if (
+    player.x + player.width > obstacle.x &&
+    player.x < obstacle.x + obstacle.width &&
+    player.y + player.height > obstacle.y &&
+    player.y < obstacle.y + obstacle.height
+  ) {
+    console.log("You collided and died!");
+  }
+}
+
 setInterval(() => {
-  render();
+  gameLoop();
 }, 20);
 
 function render() {
-  player.x += player.speed;
-  c.clearRect(0, 0, 400, 400);
-  c.fillRect(player.x, player.y, 50, 50);
+  player.speed += 0.3;
+  player.y += player.speed;
+  obstacle.x -= 2;
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  c.fillRect(player.x, player.y, player.width, player.height);
+  c.fillRect(obstacle.x, obstacle.y, obstacle.width, obstacle.height);
+  if (obstacle.x < -obstacle.width) {
+    obstacle.x = 500;
+  }
 }
 
-document.addEventListener("keydown", (event) => {
-  if (event.key === "ArrowLeft") {
-    player.speed = -1; // Move left
-  } else if (event.key === "ArrowRight") {
-    player.speed = 1; // Move right
-  } else if (event.key === "ArrowUp") {
-    player.y -= 10; // Move up
-  } else if (event.key === "ArrowDown") {
-    player.y += 10; // Move down
+window.addEventListener("keydown", (e) => {
+  if (e.key === " ") {
+    e.preventDefault();
+    player.speed = -6;
   }
 });
 
-document.addEventListener("keyup", (event) => {
-  if (event.key === "ArrowLeft" || event.key === "ArrowRight") {
-    player.speed = 0;
-  }
-});
+function gameLoop() {
+  render();
+  checkBounds();
+}
